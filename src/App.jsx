8bde981d0e87c5767210f7e2d1e9fb6486822378
Bavly Amir <167124139+Bavly-Amir@users.noreply.github.com>
2024-09-12 20:31:03 +0300

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-// this is App.jsx 
-
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import CustomNav from './components/layout/CustomNav';
 import Home from './components/Home';
 import Cards from './components/cardsComponents/Cards';
+import Cards2 from './components/cardsComponents/Cards2';
+import Cards3 from './components/cardsComponents/Cards3';  
 import ErrorPage from './components/errorPage/ErrorPage';
 import Footer from './components/layout/Footer';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import Men from './components/Men/Men';
 import Brands from './components/BrandsCart';
 import ResponsiveDrawer from './components/Casual';
-import Cards3 from './components/cardsComponents/Cards3';
-import ProductPage from './components/cardsComponents/ProductPage';  // صفحة المنتج
+import ProductPage from './components/cardsComponents/ProductPage';  
 
 function App() {
   return (
@@ -23,7 +22,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/brands" element={<Brands />} />
         <Route path="/cards" element={<Cards />} />
-        <Route path="/cards3" element={<Cards3 />} />
+        <Route path="/cards2" element={<Cards2 />} />
+        <Route path="/cards3" element={<Cards3 />} /> 
         <Route path="/product/:id" element={<ProductPage />} /> 
         <Route path="/*" element={<ErrorPage />} />
         <Route path="/men" element={<Men />} />
--- src/components/cardsComponents/Cards2.jsx
+++ src/components/cardsComponents/Cards2.jsx
@@ -1,35 +1,35 @@
-// this is cards2.jsx 
-
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Cards2() {
-    const [products, setProducts] = useState([]);
-    const [showMore, setShowMore] = useState(false);
-  
-    useEffect(() => {
-fetch('https://fakestoreapi.com/products/category/jewelery')
-          .then(res => res.json())
-        .then(json => setProducts(json));
-    }, []);
-  
-    const handleShowMore = () => {
-      setShowMore(true);
-    };
-  
-    const handleShowLess = () => {
-      setShowMore(false);
-    };
-  
-    const truncateTitle = (title) => {
-      const maxLength = 50; 
-      return title.length > maxLength ? title.substring(0, maxLength) + '...' : title;
-    };
-  
-    return (
-      <>
-        <div className="custom-card-container">
-          {products.slice(0, showMore ? products.length : 4).map(product => (
-            <div key={product.id} className="custom-card">
+  const [products, setProducts] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/category/jewelery')
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }, []);
+
+  const handleShowMore = () => {
+    setShowMore(true);
+  };
+
+  const handleShowLess = () => {
+    setShowMore(false);
+  };
+
+  const truncateTitle = (title) => {
+    const maxLength = 50;
+    return title.length > maxLength ? title.substring(0, maxLength) + '...' : title;
+  };
+
+  return (
+    <>
+      <div className="custom-card-container">
+        {products.slice(0, showMore ? products.length : 4).map(product => (
+          <div key={product.id} className="custom-card">
+            <Link to={`/product/${product.id}`}> {/* رابط يقود إلى صفحة المنتج */}
               <div className="custom-card-img-container">
                 <img src={product.image} alt={product.title} className="custom-card-img" />
               </div>
@@ -37,23 +37,24 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
                 <h5 className="custom-card-title">{truncateTitle(product.title)}</h5>
                 <p className="custom-card-price">${product.price}</p>
               </div>
-            </div>
-          ))}
-        </div>
-  
-        <div className="button-container">
-          {!showMore && (
-            <button className="show-more-button" onClick={handleShowMore}>
-              View All
-            </button>
-          )}
-  
-          {showMore && (
-            <button className="show-less-button" onClick={handleShowLess}>
-              Show Less
-            </button>
-          )}
-        </div>
-      </>
-    );
+            </Link>
+          </div>
+        ))}
+      </div>
+
+      <div className="button-container">
+        {!showMore && (
+          <button className="show-more-button" onClick={handleShowMore}>
+            View All
+          </button>
+        )}
+
+        {showMore && (
+          <button className="show-less-button" onClick={handleShowLess}>
+            Show Less
+          </button>
+        )}
+      </div>
+    </>
+  );
 }
